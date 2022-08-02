4078e7524b33c6601154c4150082fd0064b626e4
pantagonn <pantagon.intajuck@my.jcu.edu.au>
2022-08-02 20:12:28 +0800

    Refactor password check program into functions.

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -2,5 +2,22 @@
 Password Check Program
 """
 
-user_password = str(input("What is your password?: "))
-print(len(user_password) * "*")
+
+def main():
+    """Print password."""
+    password = get_password()
+    print_stars(password)
+
+
+def get_password():
+    """Get password from users."""
+    user_input = str(input("What is your password?: "))
+    return user_input
+
+
+def print_stars(length):
+    """Print the length of password in asterisks."""
+    print(len(length) * "*")
+
+
+main()
