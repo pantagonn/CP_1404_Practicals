94482ed06ae0d3af04efa69d0b0aac7ace358342
pantagonn <pantagon.intajuck@my.jcu.edu.au>
2022-08-02 21:04:21 +0800

    Add functions to generate random grade.

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -2,11 +2,15 @@
 Broken program to determine score status.
 """
 
+import random
 
 def main():
     """Get score from user and print the result."""
     score = float(input("Enter your score: "))
     print(evaluate_score(score))
+    random_grade = (random.randint(0, 100))  # generate random grade
+    print("Random grade: {}".format(random_grade))
+    print(evaluate_score(random_grade))
 
 
 def evaluate_score(score):
