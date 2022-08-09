f4e220d5b8ea5e74c5a0ad7e10830126880e15c1
pantagonn <pantagon.intajuck@my.jcu.edu.au>
2022-08-09 18:48:53 +0800

    Initial Commit

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,49 @@
+"""
+Lists warmup
+"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# What values do the following expressions have?
+
+# Q1: numbers[0]
+# A1: 3
+
+# Q2: numbers[-1]
+# A2: 2
+
+# Q3: numbers[3]
+# A3: 1
+
+# Q4: numbers[:-1]
+# A4: [3, 1, 4, 1, 5, 9]
+
+# Q5: numbers[3:4]
+# A5: [1]
+
+# Q6: 5 in numbers
+# A6: True
+
+# Q7: 7 in numbers
+# A7: False
+
+# Q8: "3" in numbers
+# A8: False
+
+# Q9: numbers + [6, 5, 3]
+# A9: [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+
+# 1. Change the first element of numbers to "ten" (the string, not the number 10)
+# A: numbers[0] = "ten"
+
+# 2. Change the last element of numbers to 1
+# A: numbers[6] = 1
+
+# 3. Get all the elements from numbers except the first two (slice)
+# A: numbers[2:]
+
+# 4. Check if 9 is an element of numbers
+# A: 9 in numbers
+
+
