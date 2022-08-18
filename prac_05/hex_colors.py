7e8b5e22ecc5de03145ee1e73d4a72d0ab8cffbc
pantagonn <pantagon.intajuck@my.jcu.edu.au>
2022-08-18 16:31:37 +0800

    Reformat program

--- prac_05/hex_colors.py
+++ prac_05/hex_colors.py
@@ -8,10 +8,10 @@ NAME_TO_CODE = {"battleshipgray": "#848482", "babypink": "#f4c2c2", "beige": "#f
                 "mandarin": "#f37a48", "rust": "#b7410e", "skobeloff": "#007474", "taupe": "#8b8589",
                 "vanillaice": "#f38fa9", "xanadu": "#738678"}
 
-color_choice = input("Please enter your desired color's name: ")
+color_choice = input("Please enter your desired color's name: ").lower()
 while color_choice != "":
     if color_choice in NAME_TO_CODE:
-        print(f"{color_choice.title()} --- {NAME_TO_CODE[color_choice]}")
+        print(f"{color_choice.title()}'s hexadecimal code is {NAME_TO_CODE[color_choice]}")
     else:
         print("Invalid color choice!")
-    color_choice = input("Please enter your desired color's name: ")
+    color_choice = input("Please enter your desired color's name: ").lower()
