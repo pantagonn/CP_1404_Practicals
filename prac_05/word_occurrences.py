a74bfd8a8101988fde934de1759d8f4dad02f6cf
pantagonn <pantagon.intajuck@my.jcu.edu.au>
2022-08-18 17:04:39 +0800

    Reformat output alignment

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -8,8 +8,12 @@ words_to_count = {}
 sentence = input("Please enter a sentence: ")
 list_of_words = sentence.split()
 list_of_words.sort()
+
 for word in list_of_words:
     word_counter = words_to_count.get(word, 0)
     words_to_count[word] = word_counter + 1
+
+maximum_word_length = max((len(word) for word in list_of_words))
+
 for word, word_count in words_to_count.items():
-    print(f"{word} : {word_count}")
+    print(f"{word:{maximum_word_length}} : {word_count}")
