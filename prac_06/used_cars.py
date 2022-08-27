9cf949032ba57222c4c84737d44cdd190800c9e9
pantagonn <pantagon.intajuck@my.jcu.edu.au>
2022-08-27 13:39:08 +0800

    Add str method and name

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -6,7 +6,7 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car(fuel=180, name='Car')
     my_car.drive(30)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
@@ -14,6 +14,14 @@ def main():
 
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+
+    print("---Limo---")
+    limo = Car(fuel=100, name='limo')
+    limo.add_fuel(20)
+    print("limo's fuel = {}".format(limo.fuel))
+    limo.drive(115)
+    print("limo's odometer = {}".format(limo.odometer))
+    print(limo)
 
 
 main()
