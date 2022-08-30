021279360e23dc17aecd25e4b9e769fc718ffdd3
pantagonn <pantagon.intajuck@my.jcu.edu.au>
2022-08-30 15:04:25 +0800

    Reformat to f-string

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -12,17 +12,14 @@ class Guitar:
         self.year = year
         self.cost = cost
 
-    def __str__(self):
-        """Return the guitar information"""
-        return '{} ({}) : ${:.2f}'.format(self.name, self.year, self.cost)
-
     def get_age(self):
         """Return the age of guitar"""
-        return 2022-self.year
+        return 2022 - self.year
 
     def is_vintage(self):
         """Check whether guitar is considered a vintage"""
         return self.get_age() >= 50
 
-
-
+    def __str__(self):
+        """Return the guitar information"""
+        return f"{self.name} ({self.year}) : ${self.cost:.2f}"
