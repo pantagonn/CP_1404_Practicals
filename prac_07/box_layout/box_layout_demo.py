a4accdfc0581fb5da2239597a9436c19a87cae75
pantagonn <pantagon.intajuck@my.jcu.edu.au>
2022-09-06 09:28:02 +0800

    Add greeting and name entering functions

--- prac_07/box_layout/box_layout_demo.py
+++ prac_07/box_layout/box_layout_demo.py
@@ -1,12 +1,23 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 
 class BoxLayoutDemo(App):
+
+    output_label = StringProperty
+
     def build(self):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
+
+    def handle_clear(self):
+        self.root.ids.output_label.text = "Enter your name"
+        self.root.ids.input_name.text = ""
+
+    def handle_greet(self):
+        self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
 
 
 BoxLayoutDemo().run()
