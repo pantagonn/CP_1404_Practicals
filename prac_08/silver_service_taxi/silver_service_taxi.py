189e754713d8064b6880796efbcd6d81b9b36f75
pantagonn <pantagon.intajuck@my.jcu.edu.au>
2022-09-13 09:42:43 +0800

    Reformat to f-string and reword the variable

--- prac_08/silver_service_taxi/silver_service_taxi.py
+++ prac_08/silver_service_taxi/silver_service_taxi.py
@@ -7,7 +7,7 @@ from prac_08.taxi.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
     """Specialized version of a Taxi that includes additional costs."""
-    flagfall = 4.5
+    flag_fall = 4.5
 
     def __init__(self, name, fuel, fanciness):
         super().__init__(name, fuel)
@@ -15,9 +15,9 @@ class SilverServiceTaxi(Taxi):
         self.price_per_km = self.price_per_km * fanciness
 
     def get_fare(self):
-        """Addition of flagfall cost and new taxi fare with fanciness"""
-        return super().get_fare() + self.flagfall
+        """Calculation of flagfall cost and new taxi fare with fanciness accounted for"""
+        return super().get_fare() + self.flag_fall
 
     def __str__(self):
-        """Overwrite __str__ in order to add a flagfall."""
-        return "{} with extra flagfall cost of ${:.2f}".format(super().__str__(), self.flagfall)
+        """Overwrite __str__ in order to add a flagfall cost."""
+        return f"{super().__str__()} with an additional flagfall cost of ${self.flag_fall:.2f}"
